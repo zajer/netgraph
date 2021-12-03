3baa33d01cfdeb090f44735636755080bb491637
zajer <pikus3@list.pl>
2021-12-03 16:48:46 +0100

    Added unit tests.

---
+++ netgraph.core/netgraph.core.tests/NetgraphTests.cs
@@ -0,0 +1,71 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using netgraph.core.library;
+namespace netgraph.core.tests
+{
+    [TestClass]
+    public class NetgraphTests
+    {
+        [TestMethod]
+        public void GetAllSubisomorphisms_NoColors_NotDirected()
+        {
+            int[][] targetAdjacency = { new int[]{2}, new int[]{0,3}, new int[]{}, new int[]{} };
+            int[][] patternAdjacency = { new int[]{1,2}, new int[]{}, new int[]{} };
+            int[] targetColors = {};
+            int[] patternColors = {};
+            Graph target = new Graph(targetAdjacency,targetColors);
+            Graph pattern = new Graph(patternAdjacency,patternColors);
+            var fact = new Factory();
+            var inet = fact.GetNetgraphProvider();
+            var result = inet.AllIsomorphisms(target,pattern,false);
+            int expectedNumberOfIsos = 4;
+            Assert.AreEqual(expectedNumberOfIsos,result.GetUpperBound(0)+1);
+        }
+        [TestMethod]
+        public void GetAllSubisomorphisms_NoColors_Directed()
+        {
+            int[][] targetAdjacency = { new int[]{2}, new int[]{0,3}, new int[]{}, new int[]{} };
+            int[][] patternAdjacency = { new int[]{1,2}, new int[]{}, new int[]{} };
+            int[] targetColors = {};
+            int[] patternColors = {};
+            Graph target = new Graph(targetAdjacency,targetColors);
+            Graph pattern = new Graph(patternAdjacency,patternColors);
+            var fact = new Factory();
+            var inet = fact.GetNetgraphProvider();
+            var result = inet.AllIsomorphisms(target,pattern,true);
+            int expectedNumberOfIsos = 2;
+            Assert.AreEqual(expectedNumberOfIsos,result.GetUpperBound(0)+1);   
+        }
+        [TestMethod]
+        public void GetAllSubisomorphisms_Colors_NotDirected()
+        {
+            int[][] targetAdjacency = { new int[]{2}, new int[]{0,3}, new int[]{}, new int[]{} };
+            int[][] patternAdjacency = { new int[]{1,2}, new int[]{}, new int[]{} };
+            int[] targetColors = {0,0,2,2};
+            int[] patternColors = {0,0,2};
+            Graph target = new Graph(targetAdjacency,targetColors);
+            Graph pattern = new Graph(patternAdjacency,patternColors);
+            var fact = new Factory();
+            var inet = fact.GetNetgraphProvider();
+            var result = inet.AllIsomorphisms(target,pattern,false);
+            int expectedNumberOfIsos = 2;
+            Assert.AreEqual(expectedNumberOfIsos,result.GetUpperBound(0)+1);
+            
+        }
+        [TestMethod]
+        public void GetAllSubisomorphisms_Colors_Directed()
+        {
+            int[][] targetAdjacency = { new int[]{2}, new int[]{0,3}, new int[]{}, new int[]{} };
+            int[][] patternAdjacency = { new int[]{1,2}, new int[]{}, new int[]{} };
+            int[] targetColors = {0,0,2,2};
+            int[] patternColors = {0,0,2};
+            Graph target = new Graph(targetAdjacency,targetColors);
+            Graph pattern = new Graph(patternAdjacency,patternColors);
+            var fact = new Factory();
+            var inet = fact.GetNetgraphProvider();
+            var result = inet.AllIsomorphisms(target,pattern,true);
+            int expectedNumberOfIsos = 1;
+            Assert.AreEqual(expectedNumberOfIsos,result.GetUpperBound(0)+1);
+            
+        }
+    }
+}
---
+++ netgraph.core/netgraph.core.tests/UtilsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace netgraph.core.tests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+        [TestMethod]
+        public void FlattenArrayOfArraysTest1()
+        {
+            int[] expectedResult = {1,7,3,99,44,12};
+            int[][] input = { new int[] { 7 }, new int[] {99,44,12}};
+            int[] result = netgraph.core.library.Utils.FlattenArrayOfArrays(input);
+            CollectionAssert.AreEqual(expectedResult,result);
+        }
+    }
+}
